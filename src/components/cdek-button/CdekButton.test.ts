4109b252af40f9897cf22036d730a22198d9a18f
d.kirichkova <d.kirichkova@cdek.ru>
2023-01-19 19:07:51 +0700

    test: cdekButton

--- src/components/cdek-button/CdekButton.test.ts
+++ src/components/cdek-button/CdekButton.test.ts
@@ -1,31 +1,65 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import { describe, test, expect } from 'vitest';
 import CdekButton from './CdekButton.vue';
 
 type ThemeT = 'primary' | 'secondary' | 'outline' | 'ghost';
 type WidthT = 'auto' | 'content';
 
-const createCdekButton = (
-  theme?: ThemeT,
-  width?: WidthT,
-  small?: boolean,
-  disabled?: boolean,
-  loading?: boolean,
-  spinnerBefore?: boolean,
-  icon?: boolean
-) => {
-  return mount(CdekButton, {
-    props: {
-      theme,
-      width,
-      small,
-      disabled,
-      loading,
-      spinnerBefore,
-      icon,
-    },
-  });
-};
+class CdekButtonBuilder {
+  theme?: ThemeT;
+  width?: WidthT;
+  small?: boolean;
+  disabled?: boolean;
+  loading?: boolean;
+  spinnerBefore?: boolean;
+  icon?: boolean;
+
+  setTheme(theme: ThemeT) {
+    this.theme = theme;
+    return this;
+  }
+  setWidth(width: WidthT) {
+    this.width = width;
+    return this;
+  }
+  toggleSmall() {
+    this.small = !this.small;
+    return this;
+  }
+  toggleDisabled() {
+    this.disabled = !this.disabled;
+    return this;
+  }
+  toggleLoading() {
+    this.loading = !this.loading;
+    return this;
+  }
+  toggleSpinnerBefore() {
+    this.spinnerBefore = !this.spinnerBefore;
+    return this;
+  }
+  toggleIcon() {
+    this.icon = !this.icon;
+    return this;
+  }
+
+  build() {
+    return shallowMount(CdekButton, {
+      props: {
+        theme: this.theme,
+        width: this.width,
+        small: this.small,
+        disabled: this.disabled,
+        loading: this.loading,
+        spinnerBefore: this.spinnerBefore,
+        icon: this.icon,
+      },
+      slots: {
+        default: 'Подтвердить',
+      },
+    });
+  }
+}
 
 describe('Unit: CdekButton', () => {
   describe('theme', () => {
@@ -38,38 +72,79 @@ describe('Unit: CdekButton', () => {
     ])(
       'Если theme = $theme, то должен быть класс $themeClass',
       ({ theme, themeClass }: any) => {
-        const wrapper = createCdekButton(theme);
-        expect(wrapper.classes(themeClass)).toBe(true);
+        const wrapper = new CdekButtonBuilder().setTheme(theme).build();
+        expect(wrapper.classes(themeClass)).toBeTruthy();
       }
     );
   });
-  describe('width', () => {
-    test.todo('Если width = "content", то должен быть класс .inline');
-    const wrapper = createCdekButton(undefined, 'content');
-    expect(wrapper.classes('inline')).toBe(true);
+
+  test('Если width = "content", то должен быть класс .inline', () => {
+    const wrapper = new CdekButtonBuilder().setWidth('content').build();
+    expect(wrapper.classes('inline')).toBeTruthy();
   });
-  describe('disabled', () => {
-    test.todo('Если disabled = true, то должен быть атрибут disabled');
-    const wrapper = createCdekButton(undefined, undefined, undefined, true);
-    // console.log(wrapper.html());
-    // expect(wrapper.attributes('disabled')).toBe(true);
+
+  test('Если disabled = true, то должен быть атрибут disabled', () => {
+    const wrapper = new CdekButtonBuilder().toggleDisabled().build();
+    expect(wrapper.attributes('disabled')).toBeDefined();
   });
-  describe('small', () => {
-    test.todo('Если small = true, то должен быть класс .small');
-    const wrapper = createCdekButton(undefined, undefined, true);
+
+  test('Если small = true, то должен быть класс .small', () => {
+    const wrapper = new CdekButtonBuilder().toggleSmall().build();
     expect(wrapper.classes('small')).toBe(true);
   });
-  describe('icon', () => {
-    test.todo('Если icon = true, то должен быть класс .icon');
-    const wrapper = createCdekButton(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      true
-    );
+
+  test('Если icon = true, то должен быть класс .icon', () => {
+    const wrapper = new CdekButtonBuilder().toggleIcon().build();
     expect(wrapper.classes('icon')).toBe(true);
+  });
+
+  describe('Loading', () => {
+    test('Если loading = false, то спиннера не должно быть', () => {
+      const wrapper = new CdekButtonBuilder().build();
+      expect(wrapper.find('cdek-spinner-stub').exists()).toBeFalsy();
+    });
+    test('Если loading = true, то должен показаться спиннер', () => {
+      const wrapper = new CdekButtonBuilder().toggleLoading().build();
+      expect(wrapper.find('cdek-spinner-stub').exists()).toBeTruthy();
+    });
+    test('Если loading = true и spinnerBefore = false, то контент должен пропасть', () => {
+      const wrapper = new CdekButtonBuilder().toggleLoading().build();
+      expect(wrapper.text()).toBe('');
+    });
+    test('Если loading = true и spinnerBefore = true, то должен показаться и спиннер, и контент', () => {
+      const wrapper = new CdekButtonBuilder()
+        .toggleLoading()
+        .toggleSpinnerBefore()
+        .build();
+      expect(wrapper.find('cdek-spinner-stub').exists()).toBeTruthy();
+      expect(wrapper.text()).toBe('Подтвердить');
+    });
+  });
+
+  describe('Цвет спиннера', () => {
+    test('Если theme = "primary", то цвет спиннера - белый', () => {
+      const wrapper = new CdekButtonBuilder().toggleLoading().build();
+      expect(wrapper.find('cdek-spinner-stub').attributes('color')).toBe(
+        'white'
+      );
+    });
+    test('Если theme = "secondary", то цвет спиннера - зеленый', () => {
+      const wrapper = new CdekButtonBuilder()
+        .setTheme('secondary')
+        .toggleLoading()
+        .build();
+      expect(wrapper.find('cdek-spinner-stub').attributes('color')).toBe(
+        'green'
+      );
+    });
+    test('Если disabled = "true", то цвет спиннера - белый', () => {
+      const wrapper = new CdekButtonBuilder()
+        .toggleLoading()
+        .toggleDisabled()
+        .build();
+      expect(wrapper.find('cdek-spinner-stub').attributes('color')).toBe(
+        'white'
+      );
+    });
   });
 });
