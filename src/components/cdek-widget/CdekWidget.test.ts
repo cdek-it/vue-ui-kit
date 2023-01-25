05e29d202d83ace9c1c0d2eea1205c5356775ccb
d.kirichkova <d.kirichkova@cdek.ru>
2023-01-25 13:24:08 +0300

    Добавлены тесты для CdekWidget

--- src/components/cdek-widget/CdekWidget.test.ts
+++ src/components/cdek-widget/CdekWidget.test.ts
@@ -2,6 +2,18 @@ import { describe, test, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import CdekWidget from './CdekWidget.vue';
 
+const createCdekWidget = (headline?: string, withoutPaddings?: boolean) => {
+  return mount(CdekWidget, {
+    props: {
+      headline,
+      withoutPaddings,
+    },
+    slots: {
+      default: 'Тест',
+    },
+  });
+};
+
 describe('Unit: CdekWidget', () => {
   test('Отображает слот', () => {
     const wrapper = mount(CdekWidget, {
@@ -10,5 +22,14 @@ describe('Unit: CdekWidget', () => {
       },
     });
     expect(wrapper.text()).toBe('Тест');
+  });
+  test('Если headline = "Заголовок", то элемент .cdek-widget__title должен содержать "Заголовок"', () => {
+    const wrapper = createCdekWidget('Заголовок');
+    const cdekHeadline = wrapper.find('.cdek-widget__title');
+    expect(cdekHeadline.text()).toBe('Заголовок');
+  });
+  test('Если withoutPaddings = true, то у элемента .cdek-widget должен присутствовать класс "cdek-widget_without-paddings"', () => {
+    const wrapper = createCdekWidget('Заголовок', true);
+    expect(wrapper.classes('cdek-widget_without-paddings')).toBeTruthy();
   });
 });
