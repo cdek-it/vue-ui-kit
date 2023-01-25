b9ee397b98f3e441f3d37d86a59062eee0a26a9c
d.kirichkova <d.kirichkova@cdek.ru>
2023-01-25 15:59:49 +0700

    add headline and paddings to cdek widget

--- src/components/cdek-widget/CdekWidget.stories.js
+++ src/components/cdek-widget/CdekWidget.stories.js
@@ -33,3 +33,46 @@ Primary.parameters = {
     },
   },
 };
+
+export const WithHeadline = Template.bind({});
+WithHeadline.args = {
+  headline: 'Название',
+  default: 'Контент',
+};
+WithHeadline.parameters = {
+  docs: {
+    source: {
+      code: '<CdekWidget headline="Название">Контент</CdekWidget>',
+    },
+  },
+};
+
+export const WithHeadlineMobile = Template.bind({});
+WithHeadlineMobile.args = {
+  headline: 'Название',
+  default: 'Контент',
+};
+WithHeadlineMobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1',
+  },
+  docs: {
+    source: {
+      code: '<CdekWidget headline="Название">Контент</CdekWidget>',
+    },
+  },
+};
+
+export const WithoutPaddings = Template.bind({});
+WithoutPaddings.args = {
+  headline: 'Название',
+  default: 'Контент',
+  withoutPaddings: true,
+};
+WithoutPaddings.parameters = {
+  docs: {
+    source: {
+      code: '<CdekWidget headline="Название" without-paddings>Контент</CdekWidget>',
+    },
+  },
+};
