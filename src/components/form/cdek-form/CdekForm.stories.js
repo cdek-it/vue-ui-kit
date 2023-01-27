7dcf43c9a131e903835572a3567f0a27c4d75c17
d.kirichkova <d.kirichkova@cdek.ru>
2023-01-27 13:53:17 +0700

    wip: cdek form basic usage

--- src/components/form/cdek-form/CdekForm.stories.js
+++ src/components/form/cdek-form/CdekForm.stories.js
@@ -1,3 +1,5 @@
+import { ref } from 'vue';
+
 import CdekForm from './CdekForm.vue';
 import { CdekFormControl } from '@/components/form/cdek-form-control';
 
@@ -10,22 +12,55 @@ export default {
 const Template = (args) => ({
   components: { CdekForm, CdekFormControl },
   setup() {
-    const submit = (values) => console.log('PARENT_SUBMIT', values);
-    const submitErrors = (values) => console.log('PARENT_ERRORS', values);
+    let form = ref({});
 
-    const rules = {
-      required: (val) => !!val,
+    const submit = (values) => {
+      form.value = values;
     };
 
-    return { args, submit, submitErrors, rules };
+    const submitErrors = (values) => console.log('PARENT_ERRORS', values);
+
+    return { args, submit, submitErrors, form };
   },
   template: `
     <CdekForm v-bind="args" @submit="submit" @submitError="submitErrors">
-      <CdekFormControl name="firstName" :rules="rules" />
-      <CdekFormControl name="surname" :rules="rules" />
+      <CdekFormControl name="firstName" />
+      <CdekFormControl name="surname" />
       <button>Продолжить</button>
     </CdekForm>
+
+    <p>Submit result: {{ form }}</p>
   `,
 });
 
 export const Primary = Template.bind({});
+Primary.args = {
+  story: 'Primary',
+};
+Primary.parameters = {
+  docs: {
+    source: {
+      code: `
+<script lang="ts" setup>
+const submit = (values) => {
+  /**
+   * В values будет лежать слепленный объект из всех CdekFormControl внутри формы
+   * Ключами выступают названия, переданные в name
+   * А значениями выступает значение поля
+   */
+  console.log(values);
+}
+</script>
+
+<template>
+  <CdekForm @submit="submit">
+    <CdekFormControl name="firstName" />
+    <CdekFormControl name="surname" />
+
+    <button>Продолжить<button>
+  </CdekForm>
+</template>
+`,
+    },
+  },
+};
