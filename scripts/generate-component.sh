8eff3d476d523b7dac349f6ed2aca44d3ec3c444
d.kirichkova <d.kirichkova@cdek.ru>
2023-01-29 05:36:50 +0700

    add script for generating component structure

---
+++ scripts/generate-component.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Название компонента
+NAME=$1
+
+NAME_DASH=$(echo $1        \
+     | sed 's/\(.\)\([A-Z]\)/\1-\2/g' \
+     | tr '[:upper:]' '[:lower:]')
+
+cd ./src/components
+mkdir ./$NAME_DASH
+
+cd ./$NAME_DASH
+
+# Vue template
+touch $NAME.vue
+cat > $NAME.vue <<- EOM
+<script lang="ts" setup>
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+EOM
+
+# Test template
+touch $NAME.test.ts
+cat > $NAME.test.ts <<- EOM
+import { shallowMount } from '@vue/test-utils';
+import { describe, test, expect } from 'vitest';
+import $NAME from './$NAME.vue';
+
+class ${NAME}Builder {
+  build() {
+    return shallowMount($NAME, {});
+  }
+}
+
+describe('Unit: $NAME', () => {
+  test('Should mount', () => {
+    const wrapper = new ${NAME}Builder().build();
+    expect(wrapper.exists()).toBeTruthy();
+  });
+});
+EOM
+
+# Stories template
+touch $NAME.stories.js
+cat > $NAME.stories.js <<- EOM
+import $NAME from './$NAME.vue';
+
+export default {
+  title: '$NAME',
+  component: $NAME,
+};
+
+const Template = (args) => ({
+  components: { $NAME },
+  setup() {
+    return { args };
+  },
+  template: \`
+    <$NAME v-bind="args" />
+  \`,
+});
+
+export const Primary = Template.bind({});
+Primary.parameters = {
+  docs: {
+    source: {
+      code: '<$NAME />',
+    },
+  },
+};
+EOM
+
+# index template
+touch index.ts
+cat > index.ts <<- EOM
+export { default as $NAME } from './$NAME.vue';
+EOM
