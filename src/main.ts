5c56510af203e898c0570b4fbe333074e0656aab
d.kirichkova <d.kirichkova@cdek.ru>
2023-04-18 14:35:55 +0700

    feat: add autocomplete to export

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ export * from './components/cdek-headline';
 export * from './components/cdek-link';
 export * from './components/cdek-widget';
 export * from './components/form';
+export * from './components/cdek-autocomplete';
