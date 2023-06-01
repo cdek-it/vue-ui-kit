a5f0c1cf4dd2f15bace375969eb0b8b967d206ce
d.kirichkova <d.kirichkova@cdek.ru>
2023-06-01 16:34:12 +0700

    fix: Исправить ошибки

--- src/components/form/cdek-form-control/CdekFormControl.vue
+++ src/components/form/cdek-form-control/CdekFormControl.vue
@@ -19,10 +19,10 @@ const props = withDefaults(
     name: string;
     rules?: RulesT;
     type?: 'text' | 'autocomplete';
-    class?: string;
+    className?: string;
     initialValue?: string;
   }>(),
-  { type: 'text', class: '', initialValue: '' }
+  { type: 'text', className: '', initialValue: '' }
 );
 
 const formService = inject(FormServiceKey) as FormService;
@@ -50,17 +50,17 @@ const value = computed({
 </script>
 
 <template>
-  <div :class="props.class">
+  <div :class="className">
     <CdekInput
       v-if="type === 'text'"
       v-model="value"
-      :valid-res="fieldService.error"
+      :valid-res="fieldService?.error"
       v-bind="$attrs"
     />
     <CdekAutocomplete
       v-if="type === 'autocomplete'"
       v-model="value"
-      :valid-res="fieldService.error"
+      :valid-res="fieldService?.error"
       v-bind="$attrs"
     />
   </div>
