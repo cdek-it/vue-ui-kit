e79e8be08e49531196f3da735a4623b2ed7ee8bf
d.kirichkova <d.kirichkova@cdek.ru>
2024-03-13 13:23:25 +0500

    #88: изменить первую страницу

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,7 +3,11 @@ const svgLoader = require('vite-svg-loader');
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/docs/1_installation.stories.mdx',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
