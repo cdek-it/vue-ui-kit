45206e21b7c5817abfab09cd068d2f9f4c428334
d.kirichkova <d.kirichkova@cdek.ru>
2024-04-02 12:06:29 +0500

    #94: Добавить поддержку кастомных компонентов через scoped slot

--- src/components/form/base-form-control/BaseFormControl.vue
+++ src/components/form/base-form-control/BaseFormControl.vue
@@ -16,10 +16,30 @@ import BaseAutocomplete from '../../base-autocomplete/BaseAutocomplete.vue';
 
 const props = withDefaults(
   defineProps<{
+    /**
+     * Под таким именем свойства результат придет в событии `submit` от `CdekForm`
+     */
     name: string;
+    /**
+     * Правила валидации, подробнее описано в [Validation](/?path=/docs/form-cdekform-validation--page) и в [GlobalValidators](/?path=/docs/form-cdekform-globalvalidators--page)
+     */
     rules?: RulesT;
+    /**
+     * - `text` CdekInput
+     * - `number` CdekInput с `type="number"`
+     * - `autocomplete` CdekAutocomplete
+     * - `slot` Кастомный компонент, подробнее в описании `default` slot
+     */
     type?: 'text' | 'number' | 'autocomplete' | 'slot';
+    /**
+     * Чтобы повесить класс на родительский `div`, используйте это свойство
+     *
+     * Чтобы класс был на самом элементе (т.е. на `CdekInput` и т.д.), используйте просто `class`
+     */
     className?: string;
+    /**
+     * Начальное значение
+     */
     initialValue?: string;
   }>(),
   { type: 'text', className: '', initialValue: '' }
@@ -48,6 +68,10 @@ const value = computed({
   },
 });
 
+const changeValue = (newValue: string) => {
+  value.value = newValue;
+};
+
 const element = computed(() => {
   if (props.type === 'autocomplete') {
     return BaseAutocomplete;
@@ -58,6 +82,10 @@ const element = computed(() => {
   }
 
   return null;
+});
+
+defineExpose({
+  changeValue,
 });
 </script>
 
@@ -73,9 +101,21 @@ const element = computed(() => {
         v-bind="$attrs"
       >
         <template v-for="(_, slot) of $slots" v-slot:[slot]="scope">
+          <!-- @slot если `type !== 'slot'`, то все слоты передаются в дочерний компонент -->
           <slot :name="slot" v-bind="scope" />
         </template>
       </component>
+    </template>
+
+    <template v-else>
+      <!--
+        @slot Если `type="slot"` или не найден подходящий элемент, то передаем в slot `value`, `changeValue` и `validRes` для кастомной обработки.
+      -->
+      <slot
+        :value="value"
+        :change-value="changeValue"
+        :valid-res="fieldService.error"
+      />
     </template>
   </div>
 </template>
