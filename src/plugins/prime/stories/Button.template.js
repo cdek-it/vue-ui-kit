267c784825e86a24cdc9ceec13391feb819eaecd
d.kirichkova <d.kirichkova@cdek.ru>
2024-11-28 16:06:36 +0500

    #114: Страничка для стилизации Button

---
+++ src/plugins/prime/stories/Button.template.js
@@ -0,0 +1,76 @@
+import Button from 'primevue/button';
+import OverlayBadge from 'primevue/overlaybadge';
+
+export { Button };
+
+export const Template = (args) => ({
+  components: { Button },
+  setup() {
+    return { args };
+  },
+  template: `
+<div :style="{ display: 'grid', gridTemplateColumns: '150px repeat(6, max-content)', gap: '15px', color: 'black', alignItems: 'center', justifyItems: 'center' }">
+  <span :style="{ justifySelf: 'flex-start' }"><code>size</code></span>
+  <span></span>
+  <span><code>rounded</code></span>
+  <span><code>variant="outlined"</code></span>
+  <span><code>variant="text"</code></span>
+  <span><code>disabled</code></span>
+  <span><code>loading</code></span>
+
+  <span :style="{ justifySelf: 'flex-start' }"><code>large</code></span>
+  <Button size="large" v-bind="args" />
+  <Button size="large" rounded v-bind="args" />
+  <Button size="large" variant="outlined" v-bind="args" />
+  <Button size="large" variant="text" v-bind="args" />
+  <Button size="large" disabled v-bind="args" />
+  <Button size="large" loading v-bind="args" />
+
+  <span :style="{ justifySelf: 'flex-start' }"><code>null</code> (default)</span>
+  <Button v-bind="args" />
+  <Button rounded v-bind="args" />
+  <Button variant="outlined" v-bind="args" />
+  <Button variant="text" v-bind="args" />
+  <Button disabled v-bind="args" />
+  <Button loading v-bind="args" />
+
+  <span :style="{ justifySelf: 'flex-start' }"><code>small</code></span>
+  <Button size="small" v-bind="args" />
+  <Button size="small" rounded v-bind="args" />
+  <Button size="small" variant="outlined" v-bind="args" />
+  <Button size="small" variant="text" v-bind="args" />
+  <Button size="small" disabled v-bind="args" />
+  <Button size="small" loading v-bind="args" />
+</div>
+`,
+});
+
+export const TemplateBadge = (args) => ({
+  components: { OverlayBadge, Button },
+  setup() {
+    return { args };
+  },
+  template: `
+<div :style="{ display: 'grid', gridTemplateColumns: 'repeat(5, max-content)', gap: '40px', rowGap: '20px', color: 'black', alignItems: 'center', justifyItems: 'center' }">
+  <span :style="{ justifySelf: 'flex-start' }"><code>size</code></span>
+  <span><code>xlarge</code></span>
+  <span><code>large</code></span>
+  <span><code>null</code> (default)</span>
+  <span><code>small</code></span>
+
+  <span></span>
+  <OverlayBadge size="xlarge" v-bind="args">
+    <Button label="Button" size="large" />
+  </OverlayBadge>
+  <OverlayBadge size="large" v-bind="args">
+    <Button label="Button" size="large" />
+  </OverlayBadge>
+  <OverlayBadge v-bind="args">
+    <Button label="Button" />
+  </OverlayBadge>
+  <OverlayBadge size="small" v-bind="args">
+    <Button label="Button" size="small" />
+  </OverlayBadge>
+</div>
+`,
+});
