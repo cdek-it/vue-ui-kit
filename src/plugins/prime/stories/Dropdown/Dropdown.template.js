538cc6d0c33fe29de890f2f6092c92d8a6b25bbd
d.kirichkova <d.kirichkova@cdek.ru>
2024-12-10 15:18:40 +0500

    #114: Перевернуть таблицу в Dropdown

--- src/plugins/prime/stories/Dropdown/Dropdown.template.js
+++ src/plugins/prime/stories/Dropdown/Dropdown.template.js
@@ -7,23 +7,25 @@ export const Template = (args) => ({
     return { args, options };
   },
   template: `
-<div :style="{ display: 'grid', gridTemplateColumns: 'repeat(5, max-content)', gap: '15px', alignItems: 'center', justifyItems: 'center' }">
+<div :style="{ display: 'grid', gridTemplateColumns: 'repeat(3, max-content)', gap: '15px', alignItems: 'center', justifyItems: 'center' }">
   <span></span>
   <span></span>
-  <span><code>invalid</code></span>
-  <span><code>loading</code></span>
-  <span><code>disabled</code></span>
+  <span><code>v-model="'Chip'"</code></span>
 
   <span></span>
   <Select :options="options" :style="{ width: '200px' }" v-bind="args" />
-  <Select :options="options" invalid :style="{ width: '200px' }" v-bind="args" />
-  <Select :options="options" loading :style="{ width: '200px' }" v-bind="args" />
-  <Select :options="options" disabled :style="{ width: '200px' }" v-bind="args" />
-
-  <span :style="{ justifySelf: 'flex-start' }"><code>v-model="'Chip'"</code></span>
   <Select :options="options" :default-value="'Chip'" :style="{ width: '200px' }" v-bind="args" />
+
+  <span :style="{ justifySelf: 'flex-start' }"><code>invalid</code></span>
+  <Select :options="options" invalid :style="{ width: '200px' }" v-bind="args" />
   <Select :options="options" invalid :default-value="'Chip'" :style="{ width: '200px' }" v-bind="args" />
+
+  <span :style="{ justifySelf: 'flex-start' }"><code>loading</code></span>
+  <Select :options="options" loading :style="{ width: '200px' }" v-bind="args" />
   <Select :options="options" loading :default-value="'Chip'" :style="{ width: '200px' }" v-bind="args" />
+
+  <span :style="{ justifySelf: 'flex-start' }"><code>disabled</code></span>
+  <Select :options="options" disabled :style="{ width: '200px' }" v-bind="args" />
   <Select :options="options" disabled :default-value="'Chip'" :style="{ width: '200px' }" v-bind="args" />
 </div>
 `,
