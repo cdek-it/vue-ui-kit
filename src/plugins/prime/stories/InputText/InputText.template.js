45ff63d5611c0e28f0e1218e448e2c28c28fe7b2
d.kirichkova <d.kirichkova@cdek.ru>
2024-12-10 15:55:54 +0500

    #114: InputText

---
+++ src/plugins/prime/stories/InputText/InputText.template.js
@@ -0,0 +1,26 @@
+import InputText from 'primevue/inputtext';
+
+export const Template = (args) => ({
+  components: { InputText },
+  setup() {
+    return { args };
+  },
+  template: `
+<div :style="{ display: 'grid', gridTemplateColumns: 'repeat(4, max-content)', gap: '15px', alignItems: 'center', justifyItems: 'center' }">
+  <span></span>
+  <span></span>
+  <span><code>invalid</code></span>
+  <span><code>disabled</code></span>
+
+  <span :style="{ justifySelf: 'flex-start' }"></span>
+  <InputText placeholder="InputText" v-bind="args" />
+  <InputText placeholder="InputText" invalid v-bind="args" />
+  <InputText placeholder="InputText" disabled v-bind="args" />
+
+  <span :style="{ justifySelf: 'flex-start' }"><code>v-model="text input"</code></span>
+  <InputText :default-value="'text input'" v-bind="args" />
+  <InputText :default-value="'text input'" invalid v-bind="args" />
+  <InputText :default-value="'text input'" disabled v-bind="args" />
+</div>
+`,
+});
