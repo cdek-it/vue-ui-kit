5e28e62f8afc4eab11b908a904ce7c3a1ff5c800
d.kirichkova <d.kirichkova@cdek.ru>
2024-12-10 15:15:26 +0500

    #114: Разбить Chips на 2 истории

--- src/plugins/prime/stories/Chips/Chips.template.js
+++ src/plugins/prime/stories/Chips/Chips.template.js
@@ -13,26 +13,22 @@ export const Template = (args) => ({
     return { args, options, complete };
   },
   template: `
-<div :style="{ display: 'grid', gridTemplateColumns: 'repeat(4, max-content)', gap: '15px', alignItems: 'center', justifyItems: 'center' }">
+<div :style="{ display: 'grid', gridTemplateColumns: 'repeat(3, max-content)', gap: '15px', alignItems: 'center', justifyItems: 'center' }">
   <span></span>
   <span></span>
   <span><code>v-model="['Chip']"</code></span>
-  <span><code>:suggestions="items"</code></span>
 
   <span></span>
-  <AutoComplete multiple :typeahead="false" v-bind="args" />
-  <AutoComplete :default-value="['Chip']" multiple :typeahead="false" v-bind="args" />
-  <AutoComplete multiple :suggestions="options" @complete="complete" v-bind="args" />
+  <AutoComplete :suggestions="options" @complete="complete" v-bind="args" />
+  <AutoComplete :default-value="['Chip']" :suggestions="options" @complete="complete" v-bind="args" />
 
   <span :style="{ justifySelf: 'flex-start' }"><code>invalid</code></span>
-  <AutoComplete invalid multiple :typeahead="false" v-bind="args" />
-  <AutoComplete :default-value="['Chip']" invalid multiple :typeahead="false" v-bind="args" />
-  <AutoComplete invalid multiple :suggestions="options" @complete="complete" v-bind="args" />
+  <AutoComplete invalid :suggestions="options" @complete="complete" v-bind="args" />
+  <AutoComplete :default-value="['Chip']" invalid :suggestions="options" @complete="complete" v-bind="args" />
 
   <span :style="{ justifySelf: 'flex-start' }"><code>disabled</code></span>
-  <AutoComplete disabled multiple :typeahead="false" v-bind="args" />
-  <AutoComplete :default-value="['Chip']" disabled multiple :typeahead="false" v-bind="args" />
-  <AutoComplete disabled multiple :suggestions="options" @complete="complete" v-bind="args" />
+  <AutoComplete disabled :suggestions="options" @complete="complete" v-bind="args" />
+  <AutoComplete :default-value="['Chip']" disabled :suggestions="options" @complete="complete" v-bind="args" />
 </div>
 `,
 });
