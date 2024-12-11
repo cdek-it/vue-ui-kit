c228257fac0a088ab417e8dc500e3921bcd798d4
d.kirichkova <d.kirichkova@cdek.ru>
2024-12-11 16:40:43 +0500

    #114: Сменить пресет на Aura

--- src/plugins/prime/install.ts
+++ src/plugins/prime/install.ts
@@ -1,13 +1,18 @@
 import merge from 'lodash/merge';
-import Lara from '@primevue/themes/lara';
+import { definePreset } from '@primevue/themes';
+import Aura from '@primevue/themes/aura';
 
 import type { PrimeVueConfiguration } from 'primevue/config';
 
+const CdekPreset = definePreset(Aura, {
+  //Your customizations, see the following sections for examples
+});
+
 const primeConfig: PrimeVueConfiguration = {
   theme: {
-    preset: Lara,
+    preset: CdekPreset,
     options: {
-      darkModeSelector: '.app-dark',
+      darkModeSelector: false,
       cssLayer: false,
     },
   },
