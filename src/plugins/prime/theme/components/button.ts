3a95cf7b7985d2803931f2558d83d88d89709a24
d.kirichkova <d.kirichkova@cdek.ru>
2024-12-26 15:33:32 +0500

    #116: button severity

--- src/plugins/prime/theme/components/button.ts
+++ src/plugins/prime/theme/components/button.ts
@@ -14,14 +14,116 @@ export default {
   colorScheme: {
     primary: {
       focusRingColor: '{primary.focus.ring.color}',
+      borderColor: '{surface.transparent}',
+      hoverBorderColor: '{surface.transparent}',
+      activeBorderColor: '{surface.transparent}',
     },
-    outlinedPrimary: {
-      hoverBackground: '{surface.transparent}',
-      activeBackground: '{surface.transparent}',
+    info: {
+      background: '{info.400}',
+      hoverBackground: '{info.500}',
+      activeBackground: '{info.400}',
+      borderColor: '{surface.transparent}',
+      hoverBorderColor: '{surface.transparent}',
+      activeBorderColor: '{surface.transparent}',
+      focusRingColor: '{primary.focus.ring.color}',
     },
-    textPrimary: {
-      hoverBackground: '{primary.alpha}',
-      activeBackground: '{surface.transparent}',
+    success: {
+      background: '{success.400}',
+      hoverBackground: '{success.500}',
+      activeBackground: '{success.400}',
+      borderColor: '{surface.transparent}',
+      hoverBorderColor: '{surface.transparent}',
+      activeBorderColor: '{surface.transparent}',
+      focusRingColor: '{primary.focus.ring.color}',
+    },
+    warn: {
+      background: '{warning.400}',
+      hoverBackground: '{warning.500}',
+      activeBackground: '{warning.400}',
+      borderColor: '{surface.transparent}',
+      hoverBorderColor: '{surface.transparent}',
+      activeBorderColor: '{surface.transparent}',
+      focusRingColor: '{primary.focus.ring.color}',
+    },
+    danger: {
+      background: '{danger.400}',
+      hoverBackground: '{danger.500}',
+      activeBackground: '{danger.400}',
+      borderColor: '{surface.transparent}',
+      hoverBorderColor: '{surface.transparent}',
+      activeBorderColor: '{surface.transparent}',
+      focusRingColor: '{primary.focus.ring.color}',
+    },
+    help: {
+      background: '{help.400}',
+      hoverBackground: '{help.500}',
+      activeBackground: '{help.400}',
+      borderColor: '{surface.transparent}',
+      hoverBorderColor: '{surface.transparent}',
+      activeBorderColor: '{surface.transparent}',
+      focusRingColor: '{primary.focus.ring.color}',
+    },
+    outlined: {
+      primary: {
+        hoverBackground: '{surface.transparent}',
+        activeBackground: '{surface.transparent}',
+      },
+      info: {
+        color: '{info.400}',
+        borderColor: '{info.200}',
+        hoverBackground: '{surface.transparent}',
+        activeBackground: '{surface.transparent}',
+      },
+      success: {
+        color: '{success.400}',
+        borderColor: '{success.200}',
+        hoverBackground: '{surface.transparent}',
+        activeBackground: '{surface.transparent}',
+      },
+      warn: {
+        color: '{warning.400}',
+        borderColor: '{warning.200}',
+        hoverBackground: '{surface.transparent}',
+        activeBackground: '{surface.transparent}',
+      },
+      danger: {
+        color: '{danger.400}',
+        borderColor: '{danger.200}',
+        hoverBackground: '{surface.transparent}',
+        activeBackground: '{surface.transparent}',
+      },
+      help: {
+        color: '{help.400}',
+        borderColor: '{help.200}',
+        hoverBackground: '{surface.transparent}',
+        activeBackground: '{surface.transparent}',
+      },
+    },
+    text: {
+      primary: {
+        hoverBackground: '{primary.alpha}',
+        activeBackground: '{surface.transparent}',
+      },
+      info: {
+        hoverBackground: '{info.alpha}',
+        activeBackground: '{surface.transparent}',
+      },
+      success: {
+        hoverBackground: '{success.alpha}',
+        activeBackground: '{surface.transparent}',
+      },
+      warn: {
+        hoverBackground: '{warning.alpha}',
+        activeBackground: '{surface.transparent}',
+      },
+      danger: {
+        hoverBackground: '{danger.alpha}',
+        activeBackground: '{surface.transparent}',
+      },
+      help: {
+        hoverBackground: '{help.alpha}',
+        activeBackground: '{surface.transparent}',
+      },
     },
   },
   css: ({ dt }: { dt: (token: string) => string }) => `
@@ -70,7 +172,11 @@ export default {
     }
 
     .p-button-outlined:not(:disabled):not(:active):hover {
-      border-color: ${dt('primary.color')};
-    }
+      --p-button-outlined-primary-border-color: ${dt('primary.color')};
+      --p-button-outlined-info-border-color: ${dt('info.400')};
+      --p-button-outlined-success-border-color: ${dt('success.400')};
+      --p-button-outlined-warn-border-color: ${dt('warning.400')};
+      --p-button-outlined-danger-border-color: ${dt('danger.400')};
+      --p-button-outlined-help-border-color: ${dt('help.400')};
   `,
 };
