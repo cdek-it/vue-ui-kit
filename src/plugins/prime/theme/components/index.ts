ae038efbbb8ba4da2e407b14e56335c42476999f
d.kirichkova <d.kirichkova@cdek.ru>
2024-12-26 16:31:48 +0500

    #116: Checkbox

--- src/plugins/prime/theme/components/index.ts
+++ src/plugins/prime/theme/components/index.ts
@@ -1,9 +1,11 @@
 import badge from './badge';
 import button from './button';
+import checkbox from './checkbox';
 import overlaybadge from './overlayBadge';
 
 export default {
   badge,
   button,
+  checkbox,
   overlaybadge,
 };
--- src/plugins/prime/theme/semantic.ts
+++ src/plugins/prime/theme/semantic.ts
@@ -8,10 +8,18 @@ export default {
   },
   colorScheme: {
     ...semanticColors,
+    focusRingColor: '{primary.200}',
     primary: {
       color: '{primary.700}',
       focusRingColor: '{primary.200}',
       hoverColor: '{primary.800}',
+    },
+    formField: {
+      background: '{surface.ground}',
+      disabledBackground: '{black.10}',
+      borderColor: '{surface.border}',
+      hoverBorderColor: '{primary.color}',
+      invalidBorderColor: '{danger.400}',
     },
   },
   focusRing: {
