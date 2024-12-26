7dd5d6071ac570d03328c9994e9cd6557ce83ab0
d.kirichkova <d.kirichkova@cdek.ru>
2024-12-26 14:35:13 +0500

    #116: Badge

--- src/plugins/prime/theme/components/badge.ts
+++ src/plugins/prime/theme/components/badge.ts
@@ -1,7 +1,10 @@
 export default {
   borderRadius: '100px',
   colorScheme: {
-    dangerColor: '{white.100}',
+    danger: {
+      color: '{white.100}',
+      background: '{danger.400}',
+    },
     info: {
       color: '{white.100}',
       background: '{info.400}',
@@ -9,6 +12,10 @@ export default {
     success: {
       color: '{white.100}',
       background: '{success.400}',
+    },
+    warn: {
+      color: '{white.100}',
+      background: '{warning.400}',
     },
   },
 };
