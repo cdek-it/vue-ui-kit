9552917fb48e45242f1bf26ed03a5901d2b87428
Sergey Kosov <s.kosov@cdek.ru>
2025-02-17 16:20:41 +0700

    #117: icon tests

--- src/primeBlocks/PBlockToggleButton/PBlockToggleButton.test.ts
+++ src/primeBlocks/PBlockToggleButton/PBlockToggleButton.test.ts
@@ -87,8 +87,6 @@ describe('Unit: PBlockToggleButton', () => {
 
     await label.trigger('click');
 
-    console.log('label', label.html());
-
     expect(wrapper.classes().includes('p-togglebutton-checked')).toBeTruthy();
 
     expect(wrapper.text()).toContain('On Label');
@@ -105,8 +103,6 @@ describe('Unit: PBlockToggleButton', () => {
     expect(label.text()).toContain('Off Label');
 
     await label.trigger('click');
-
-    console.log('label', label.html());
 
     expect(wrapper.classes().includes('p-togglebutton-checked')).toBeTruthy();
 
@@ -125,8 +121,6 @@ describe('Unit: PBlockToggleButton', () => {
 
     await label.trigger('click');
 
-    console.log('label', label.html());
-
     expect(wrapper.classes().includes('p-togglebutton-checked')).toBeTruthy();
 
     expect(wrapper.text()).toContain('On Label');
@@ -138,6 +132,104 @@ describe('Unit: PBlockToggleButton', () => {
       .build();
 
     expect(wrapper.find('.ti-arrow-down-right').exists()).toBeTruthy();
+  });
+
+  test('Если задана baseIcon, то должна отображаться заданная иконка и при нажатии', async () => {
+    const wrapper = new PBlockToggleButtonBuilder()
+      .setBaseIcon('ti ti-arrow-down-right')
+      .build();
+
+    const icon = wrapper.find('.p-togglebutton-icon');
+
+    expect(icon.exists()).toBeTruthy();
+    expect(wrapper.find('.ti-arrow-down-right').exists()).toBeTruthy();
+
+    await icon.trigger('click');
+
+    expect(wrapper.find('.ti-arrow-down-right').exists()).toBeTruthy();
+  });
+
+  test('Если задана baseIcon и onIcon, то при нажатии отображается OnIcon', async () => {
+    const wrapper = new PBlockToggleButtonBuilder()
+      .setBaseIcon('ti ti-arrow-down-right')
+      .setOnIcon('ti ti-star')
+      .build();
+
+    const icon = wrapper.find('.p-togglebutton-icon');
+
+    expect(icon.exists()).toBeTruthy();
+    expect(wrapper.find('.ti-arrow-down-right').exists()).toBeTruthy();
+
+    await icon.trigger('click');
+
+    expect(wrapper.find('.ti-star').exists()).toBeTruthy();
+  });
+
+  test('Если задана offIcon, то должна отображаться по умолчанию', () => {
+    const wrapper = new PBlockToggleButtonBuilder()
+      .setOffIcon('ti ti-lock')
+      .build();
+
+    const icon = wrapper.find('.p-togglebutton-icon');
+
+    expect(icon.exists()).toBeTruthy();
+    expect(wrapper.find('.ti-lock').exists()).toBeTruthy();
+  });
+
+  test('Если задана onIcon, то она отображается после нажатия', async () => {
+    const wrapper = new PBlockToggleButtonBuilder()
+      .setOnIcon('ti ti-unlock')
+      .build();
+
+    const icon = wrapper.find('.p-togglebutton-icon');
+
+    expect(icon.exists()).toBeTruthy();
+    expect(wrapper.find('.ti-unlock').exists()).toBeFalsy();
+
+    await icon.trigger('click');
+
+    expect(wrapper.find('.ti-unlock').exists()).toBeTruthy();
+  });
+
+  test('Если задана offIcon, она отображается снова после повторного нажатия', async () => {
+    const wrapper = new PBlockToggleButtonBuilder()
+      .setOffIcon('ti ti-lock')
+      .setOnIcon('ti ti-unlock')
+      .build();
+
+    const icon = wrapper.find('.p-togglebutton-icon');
+
+    expect(wrapper.find('.ti-lock').exists()).toBeTruthy();
+
+    await icon.trigger('click');
+
+    expect(wrapper.find('.ti-unlock').exists()).toBeTruthy();
+
+    await icon.trigger('click');
+
+    expect(wrapper.find('.ti-lock').exists()).toBeTruthy();
+  });
+
+  test('Если заданы baseIcon, onIcon и offIcon, то отображаются onIcon и offIcon', async () => {
+    const wrapper = new PBlockToggleButtonBuilder()
+      .setBaseIcon('ti ti-arrow-down-right')
+      .setOnIcon('ti ti-star')
+      .setOffIcon('ti ti-lock')
+      .build();
+
+    const icon = wrapper.find('.p-togglebutton-icon');
+
+    expect(wrapper.find('.ti-lock').exists()).toBeTruthy();
+
+    // Нажатие для перехода в активное состояние
+    await icon.trigger('click');
+
+    // Проверка отображения baseIcon и onIcon
+    expect(wrapper.find('.ti-star').exists()).toBeTruthy();
+
+    // Повторное нажатие возвращает исходные иконки
+    await icon.trigger('click');
+    expect(wrapper.find('.ti-lock').exists()).toBeTruthy();
   });
 
   //
