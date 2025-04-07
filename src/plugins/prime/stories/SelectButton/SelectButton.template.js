af39439f6e995200eb843b9fe595fd440ff3c3ee
serega <s.esipenko@gmail.com>
2025-04-07 23:40:03 +0700

    125: бекграунд для селект батона

--- src/plugins/prime/stories/SelectButton/SelectButton.template.js
+++ src/plugins/prime/stories/SelectButton/SelectButton.template.js
@@ -8,7 +8,12 @@ export const Template = (args) => ({
       { name: 'Option 2', value: '2' },
     ];
 
-    return { args, options };
+    const semiDisabledOption = [
+      { name: 'Option 1', value: '1', disabled: false },
+      { name: 'Option 2', value: '2', disabled: true },
+      { name: 'Option 3', value: '3', disabled: true },
+    ];
+    return { args, options, semiDisabledOption };
   },
   template: `
 <div :style="{ display: 'grid', gridTemplateColumns: 'repeat(3, max-content)', gap: '15px', alignItems: 'center', justifyItems: 'center' }">
@@ -28,6 +33,14 @@ export const Template = (args) => ({
   <span><code>size="large"</code></span>
   <SelectButton size="large" :options="options" optionLabel="name" optionValue="value" :default-value="options[0].value" v-bind="args" />
   <SelectButton size="large" :options="options" optionLabel="name" optionValue="value" :default-value="options[0].value" disabled v-bind="args" />
+  
+  <span>
+    <code>
+      semi-disabled
+    </code>
+  </span>
+
+  <SelectButton size="large" :options="semiDisabledOption" optionLabel="name" optionValue="value" :default-value="options[0].value" v-bind="args" :optionDisabled="(e) => e.disabled"/>
 </div>
 `,
 });
