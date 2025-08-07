1384e707f40f0bad45e15711fdc38f63f881fc02
Sergey Kosov <sergeykosoff@gmail.com>
2025-08-07 15:29:01 +0700

    feat: noModal story

--- src/plugins/prime/stories/Dialog/Dialog.template.js
+++ src/plugins/prime/stories/Dialog/Dialog.template.js
@@ -5,13 +5,12 @@ import Button from 'primevue/button';
 export const Template = (args) => ({
   components: { Dialog, Button },
   setup() {
-    const visible = ref(false);
+    const visible = ref(args.modal === false);
 
     return { args, visible };
   },
   template: `
-    <Button @click="visible = true">Нажми на меня</Button>
-    
+    <Button v-if="args.modal !== false" @click="visible = true">Нажми на меня</Button>
     
     <Dialog header="Dialog" modal v-model:visible="visible" v-bind="args">
       <div> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, saepe. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, saepe. </div>
