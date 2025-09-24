7b137e3b19a606c2f0f2a86c1b49fc8e38ba8687
anatoly-rodin <anatoly.richman@gmail.com>
2025-09-24 12:14:13 +0300

    #43: Добавлена story для аватара с изображением.

--- src/plugins/prime/stories/Misc/Avatar/Avatar.template.js
+++ src/plugins/prime/stories/Misc/Avatar/Avatar.template.js
@@ -69,6 +69,40 @@ export const AvatarIconTemplate = (args) => ({
   `,
 });
 
+export const AvatarImageTemplate = (args) => ({
+  components: { Avatar },
+  setup() {
+    const containerStyles = {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(3, max-content)',
+      gap: '15px 35px',
+      alignItems: 'center',
+      justifyItems: 'center',
+    };
+
+    return { args, containerStyles };
+  },
+  template: `
+      <div :style="containerStyles">
+      <span></span>
+      <span></span>
+      <span>rounded</span>
+
+      <span :style="{ justifySelf: 'flex-start' }"><code>size="xlarge"</code></span>
+      <Avatar image="assets/avatar.png" size="xlarge" />
+      <Avatar image="assets/avatar.png" size="xlarge" shape="circle" />
+      
+      <span :style="{ justifySelf: 'flex-start' }"><code>size="large"</code></span>
+      <Avatar image="assets/avatar.png" size="large" />
+      <Avatar image="assets/avatar.png" size="large" shape="circle" />
+      
+      <span></span>
+      <Avatar image="assets/avatar.png" />
+      <Avatar image="assets/avatar.png" shape="circle" />
+    </div>
+  `,
+});
+
 export const AvatarBadgeTemplate = (args) => ({
   components: { Avatar, OverlayBadge },
   setup() {
@@ -156,6 +190,52 @@ export const AvatarIconBadgeTemplate = (args) => ({
       </OverlayBadge>
       <OverlayBadge value="Badge" severity="danger">
         <Avatar icon="ti ti-user" shape="circle" />
+      </OverlayBadge>
+    </div>
+  `,
+});
+
+export const AvatarImageBadgeTemplate = (args) => ({
+  components: { Avatar, OverlayBadge },
+  setup() {
+    const containerStyles = {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(3, max-content)',
+      gap: '15px 55px',
+      alignItems: 'center',
+      justifyItems: 'center',
+    };
+
+    return { args, containerStyles };
+  },
+  template: `
+    <div :style="containerStyles">
+      <span></span>
+      <span></span>
+      <span>rounded</span>
+
+      <span :style="{ justifySelf: 'flex-start' }"><code>size="xlarge"</code></span>
+      <OverlayBadge value="Badge" severity="primary">
+        <Avatar image="assets/avatar.png" size="xlarge" />
+      </OverlayBadge>
+      <OverlayBadge value="Badge" severity="info">
+        <Avatar image="assets/avatar.png" size="xlarge" shape="circle" />
+      </OverlayBadge>
+      
+      <span :style="{ justifySelf: 'flex-start' }"><code>size="large"</code></span>
+      <OverlayBadge value="Badge" severity="secondary">
+        <Avatar image="assets/avatar.png" size="large" />
+      </OverlayBadge>
+      <OverlayBadge value="Badge" severity="warn">
+        <Avatar image="assets/avatar.png" size="large" shape="circle" />
+      </OverlayBadge>
+      
+      <span></span>
+      <OverlayBadge value="Badge" severity="info">
+        <Avatar image="assets/avatar.png" />
+      </OverlayBadge>
+      <OverlayBadge value="Badge" severity="danger">
+        <Avatar image="assets/avatar.png" shape="circle" />
       </OverlayBadge>
     </div>
   `,
