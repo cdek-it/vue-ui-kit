e65efdcc3463ac652b4505b2c5ea76ba912ca6c2
Sergey Kosov <sergeykosoff@gmail.com>
2025-12-12 10:33:55 +0700

    DS-157: фиксы для link и text вариантов кнопки с иконкой

--- src/plugins/prime/theme3.0/components/css/button.ts
+++ src/plugins/prime/theme3.0/components/css/button.ts
@@ -36,6 +36,33 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
 .p-button.p-button-link:not(:disabled):hover .p-button-label {
   text-decoration: none;
 }
+
+.p-button-lg.p-button-icon-only.p-button-link {
+  width: ${dt('button.extend.extLink.lg.iconOnlyWidth')};
+  height: ${dt('button.extend.extLink.lg.iconOnlyWidth')};
+}
+
+.p-button-link.p-button-icon-only.p-button{
+  width: ${dt('button.extend.extLink.base.iconOnlyWidth')};
+  height: ${dt('button.extend.extLink.base.iconOnlyWidth')};
+}
+
+.p-button-link.p-button-icon-only.p-button-sm{
+  width: ${dt('button.extend.extLink.sm.iconOnlyWidth')};
+  height: ${dt('button.extend.extLink.sm.iconOnlyWidth')};
+}
+
+.p-button-lg.p-button-icon-only {
+  height: ${dt('button.root.lg.iconOnlyWidth')};
+}
+
+.p-button-icon-only {
+  height: ${dt('button.root.iconOnlyWidth')};
+}
+
+.p-button-sm.p-button-icon-only {
+  height: ${dt('button.root.iconOnlyWidth')};
+}
 `;
 
 export default css;
