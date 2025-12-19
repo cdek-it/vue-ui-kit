b929ed1a78387b341e6c3fb1dd420d6e6bf96fd4
Danil Khaliulin <danielooneilie@gmail.com>
2025-12-19 15:10:33 +0300

    breadcrumb

--- src/plugins/prime/theme3.0/semantic.ts
+++ src/plugins/prime/theme3.0/semantic.ts
@@ -183,6 +183,11 @@ export default {
       shadow: '0 2px 12px 0 rgba(0, 0, 0, 0.1)',
     },
   },
+  opacity: {
+    default: '{opacity.100}',
+    muted: '{opacity.50}',
+    disabled: '{opacity.0}',
+  },
   transitionDuration: '0.2s',
   disabledOpacity: '0.5',
   iconSizeMedium: '1rem',
