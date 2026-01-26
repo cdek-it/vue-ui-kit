67d3e062b3494cae53bf020702350da0b5c356e7
Danil Khaliulin <danielooneilie@gmail.com>
2026-01-26 15:48:12 +0300

    progress-bar: типографика

--- src/plugins/prime/theme3.0/components/css/progressbar.ts
+++ src/plugins/prime/theme3.0/components/css/progressbar.ts
@@ -1,9 +1,6 @@
 const css = ({ dt }: { dt: (token: string) => string }) => `
 .p-progressbar-label {
   font-family: ${dt('fonts.fontFamily.body')};
-  font-size: ${dt('progressbar.label.fontSize')};
-  font-weight: ${dt('progressbar.label.fontWeight')};
-  color: ${dt('progressbar.label.color')};
 }
 `;
 
