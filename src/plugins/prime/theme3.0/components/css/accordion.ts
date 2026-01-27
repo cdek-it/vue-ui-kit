bf07655fb62fc41a20bf2e6f1ebb186efcf82165
Danil Khaliulin <danielooneilie@gmail.com>
2026-01-27 16:31:04 +0300

    accordion: апдейт типографики

--- src/plugins/prime/theme3.0/components/css/accordion.ts
+++ src/plugins/prime/theme3.0/components/css/accordion.ts
@@ -1,18 +1,19 @@
 const css = ({ dt }: { dt: (token: string) => string }) => `
 
+/* Стилизация текста заголовка */
+.p-accordionheader {
+  font-family: ${dt('fonts.fontFamily.base')};
+  font-size: ${dt('fonts.fontSize.base')};
+}
 
 /* Иконка chevron перемещается в конец */
 .p-accordionheader-toggle-icon {
   font-size: ${dt('accordion.extend.extHeader.iconSize')};
-  width: ${dt('accordion.extend.extHeader.iconSize')};
-  height: ${dt('accordion.extend.extHeader.iconSize')};
 }
 
 /* Иконка chevron в активном состоянии */
 .p-accordionpanel:not(.p-disabled).p-accordionpanel-active > .p-accordionheader .p-accordionheader-toggle-icon {
   font-size: ${dt('accordion.extend.extHeader.iconSize')};
-  width: ${dt('accordion.extend.extHeader.iconSize')};
-  height: ${dt('accordion.extend.extHeader.iconSize')};
 }
 
 /* Кастомная структура с иконкой в хедере */
@@ -25,8 +26,6 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
 /* Размер кастомных иконок в заголовке */
 .p-accordionheader > div > i {
   font-size: ${dt('accordion.extend.extHeader.iconSize')};
-  width: ${dt('accordion.extend.extHeader.iconSize')};
-  height: ${dt('accordion.extend.extHeader.iconSize')};
 }
 `;
 
