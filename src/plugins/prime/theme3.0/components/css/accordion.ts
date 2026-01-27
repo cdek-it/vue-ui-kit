af01cbbd42efc555254608911be73239a8b36e6f
Danil Khaliulin <danielooneilie@gmail.com>
2026-01-27 17:18:20 +0300

    оптимизация файла css

--- src/plugins/prime/theme3.0/components/css/accordion.ts
+++ src/plugins/prime/theme3.0/components/css/accordion.ts
@@ -1,32 +1,18 @@
-const css = ({ dt }: { dt: (token: string) => string }) => `
+export default ({ dt }: { dt: (token: string) => string }) => `
+  .p-accordionheader {
+    font-family: ${dt('fonts.fontFamily.base')};
+    font-size: ${dt('fonts.fontSize.base')};
+  }
 
-/* Стилизация текста заголовка */
-.p-accordionheader {
-  font-family: ${dt('fonts.fontFamily.base')};
-  font-size: ${dt('fonts.fontSize.base')};
-}
+  .p-accordionheader-toggle-icon,
+  .p-accordionpanel:not(.p-disabled).p-accordionpanel-active > .p-accordionheader .p-accordionheader-toggle-icon,
+  .p-accordionheader > div > i {
+    font-size: ${dt('accordion.extend.extHeader.iconSize')};
+  }
 
-/* Иконка chevron перемещается в конец */
-.p-accordionheader-toggle-icon {
-  font-size: ${dt('accordion.extend.extHeader.iconSize')};
-}
-
-/* Иконка chevron в активном состоянии */
-.p-accordionpanel:not(.p-disabled).p-accordionpanel-active > .p-accordionheader .p-accordionheader-toggle-icon {
-  font-size: ${dt('accordion.extend.extHeader.iconSize')};
-}
-
-/* Кастомная структура с иконкой в хедере */
-.p-accordionheader > div {
-  display: flex;
-  align-items: center;
-  gap: ${dt('accordion.extend.extHeader.gap')};
-}
-
-/* Размер кастомных иконок в заголовке */
-.p-accordionheader > div > i {
-  font-size: ${dt('accordion.extend.extHeader.iconSize')};
-}
+  .p-accordionheader > div {
+    display: flex;
+    align-items: center;
+    gap: ${dt('accordion.extend.extHeader.gap')};
+  }
 `;
-
-export default css;
