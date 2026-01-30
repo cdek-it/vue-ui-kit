f88e92a7ffe97dbeac3d711734564c5cfe32ebfd
Danil Khaliulin <danielooneilie@gmail.com>
2026-01-30 13:01:02 +0300

    оптимизация файла стилей

--- src/plugins/prime/theme3.0/components/css/panelmenu.ts
+++ src/plugins/prime/theme3.0/components/css/panelmenu.ts
@@ -1,5 +1,5 @@
 const css = ({ dt }: { dt: (token: string) => string }) => `
-
+/* Базовые стили PanelMenu */
 .p-panelmenu {
   gap: ${dt('panelmenu.extend.extPanel.gap')};
 }
@@ -8,7 +8,9 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
   padding: ${dt('panelmenu.extend.extPanel.gap')};
 }
 
-.p-panelmenu-header-content {
+/* Типографика */
+.p-panelmenu-header-content,
+.p-panelmenu-item-content {
   font-size: ${dt('fonts.fontSize.base')};
 }
 
@@ -16,18 +18,17 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
   font-size: ${dt('panelmenu.extend.iconSize')};
 }
 
-.p-panelmenu-item-content {
-  font-size: ${dt('fonts.fontSize.base')};
-}
-
+/* Активное состояние для элементов без подменю */
 .p-panelmenu-item:not(:has(.p-panelmenu-submenu-icon)).p-menuitem-checked > .p-panelmenu-item-content .p-panelmenu-item-icon {
   color: ${dt('panelmenu.extend.extItem.icon.activeColor')};
 }
 
+/* Сохранение цвета иконки подменю для элементов с подменю */
 .p-panelmenu-item:has(.p-panelmenu-submenu-icon).p-menuitem-checked > .p-panelmenu-item-content .p-panelmenu-submenu-icon {
   color: ${dt('panelmenu.submenuIcon.color')};
 }
 
+/* Стили для подписей */
 .p-panelmenu-item-label:has(.p-panelmenu-item-caption) {
   display: flex;
   flex-direction: column;
