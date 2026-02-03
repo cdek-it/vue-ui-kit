427eb2d8b637d8ce52265d08a05cf7ce2943c1c7
Danil Khaliulin <danielooneilie@gmail.com>
2026-02-03 19:02:52 +0300

    оптимизация файла стилей

--- src/plugins/prime/theme3.0/components/css/inputotp.ts
+++ src/plugins/prime/theme3.0/components/css/inputotp.ts
@@ -1,20 +1,8 @@
 const css = ({ dt }: { dt: (token: string) => string }) => `
-/* Базовые стили для InputOtp */
-.p-inputotp {
-  gap: ${dt('inputotp.root.gap')};
-}
 
-/* Стили для каждого input внутри InputOtp */
+/* Стили границы */
 .p-inputotp .p-inputtext {
-  width: ${dt('inputotp.input.width')};
   border-width: ${dt('inputotp.extend.borderWidth')};
-  text-align: center;
-  line-height: ${dt('fonts.lineHeight.25')};
-}
-
-/* Центрирование текста в input */
-.p-inputotp .p-inputtext::placeholder {
-  text-align: center;
 }
 `;
 
