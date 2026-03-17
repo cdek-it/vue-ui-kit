bf585daa6afcae35a4e3657ef1267dd8e63eaa30
Sergey Kosov <49809485+Skosov@users.noreply.github.com>
2026-03-17 15:58:32 +0700

    Revert "стилизация; сторисы (#133)"
    This reverts commit 18b91968ea3c2f4c1558efddcea5dbd35ea768c8.

--- src/plugins/prime/theme3.0/components/css/avatar.ts
+++ src/plugins/prime/theme3.0/components/css/avatar.ts
@@ -1,22 +1,6 @@
 const css = ({ dt }: { dt: (token: string) => string }) => `
-.p-avatar.p-component .p-avatar-text {
-  font-family: ${dt('fonts.fontFamily.heading')};
-  font-weight: ${dt('fonts.fontWeight.demibold')};
-  font-size: ${dt('avatar.root.fontSize')};
-  line-height: normal;
-}
-
-.p-avatar-group[data-pc-name="avatargroup"] .p-avatar[data-pc-name="avatar"] {
-  border: ${dt('borderWidth.200')} solid ${dt('avatar.group.borderColor')};
-}
-
-.p-avatar-group[data-pc-name="avatargroup"] .p-avatar[data-pc-name="avatar"] + .p-avatar[data-pc-name="avatar"] {
-  margin-left: calc(-1 * ${dt('avatar.group.offset')});
-}
-
-/* XL offset override */
-.p-avatar-group[data-pc-name="avatargroup"] .p-avatar-xl[data-pc-name="avatar"] + .p-avatar-xl[data-pc-name="avatar"] {
-  margin-left: calc(-1 * ${dt('avatar.xl.group.offset')});
+.p-avatar.p-component {
+  font-weight: ${dt('fonts.fontWeight.bold')};
 }
 `;
 
