0177a42e417861f1b1aca8f89d19f5cf100dc448
Danil Khaliulin <danielooneilie@gmail.com>
2026-03-18 14:14:12 +0700

    панель пропсов

--- src/plugins/prime/stories/Misc/Tag/Tag.template.js
+++ src/plugins/prime/stories/Misc/Tag/Tag.template.js
@@ -7,3 +7,20 @@ export const Template = (args) => ({
   },
   template: '<Tag v-bind="args" />',
 });
+
+export const TemplateSeverities = (args) => ({
+  components: { Tag },
+  setup() {
+    return { args };
+  },
+  template: `
+    <div class="flex flex-wrap items-center gap-4">
+      <Tag value="Primary" severity="primary" />
+      <Tag value="Secondary" severity="secondary" />
+      <Tag value="Success" severity="success" />
+      <Tag value="Info" severity="info" />
+      <Tag value="Warn" severity="warn" />
+      <Tag value="Danger" severity="danger" />
+    </div>
+  `,
+});
