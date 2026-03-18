0e8bde97198df112efa633d2679e1b905c7f30d9
Danil Khaliulin <danielooneilie@gmail.com>
2026-03-18 13:12:04 +0700

    стилизация компонента

--- src/plugins/prime/theme3.0/components/css/rating.ts
+++ src/plugins/prime/theme3.0/components/css/rating.ts
@@ -3,12 +3,13 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
 .p-rating-wrapper {
   display: flex;
   align-items: center;
-  gap: ${dt('form.gap.xs')};
+  gap: ${dt('form.gap.200')};
 }
 
 .p-rating-wrapper > :is(i, [class*="icon"]) {
-  font-size: ${dt('form.icon.xlg')};
+  font-size: ${dt('form.icon.500')};
 }
+
 `;
 
 export default css;
--- src/plugins/prime/theme3.0/css.ts
+++ src/plugins/prime/theme3.0/css.ts
@@ -7,7 +7,6 @@ import dialogCss from './components/css/dialog';
 import dividerCss from './components/css/divider';
 import drawerCss from './components/css/drawer';
 import progressbarCss from './components/css/progressbar';
-import ratingCss from './components/css/rating';
 import tagCss from './components/css/tag';
 import tooltipCss from './components/css/tooltip';
 
@@ -21,7 +20,6 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
    ${dividerCss({ dt })}
    ${drawerCss({ dt })}
    ${progressbarCss({ dt })}
-   ${ratingCss({ dt })}
    ${tagCss({ dt })}
    ${tooltipCss({ dt })}
 
