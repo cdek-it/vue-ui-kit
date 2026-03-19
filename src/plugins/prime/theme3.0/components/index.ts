2a3b8986f2f69509a52155c6dedad72592bf26cb
Danil Khaliulin <danielooneilie@gmail.com>
2026-03-19 21:06:51 +0700

    accordion: стилизация, сторисы

--- src/plugins/prime/theme3.0/components/index.ts
+++ src/plugins/prime/theme3.0/components/index.ts
@@ -53,7 +53,7 @@ export default {
     },
     panel: {
       borderWidth:
-        '{borderWidth.none} {borderWidth.none} {navigation.width.200} {borderWidth.none}',
+        '{borderWidth.none} {borderWidth.none} {navigation.width.100} {borderWidth.none}',
       borderColor: '{form.borderColor}',
     },
     content: {
--- src/plugins/prime/theme3.0/css.ts
+++ src/plugins/prime/theme3.0/css.ts
@@ -1,3 +1,4 @@
+import accordionCss from './components/css/accordion';
 import avatarCss from './components/css/avatar';
 import badgeCss from './components/css/badge';
 import breadcrumbCss from './components/css/breadcrumb';
@@ -12,6 +13,7 @@ import tagCss from './components/css/tag';
 import tooltipCss from './components/css/tooltip';
 
 const css = ({ dt }: { dt: (token: string) => string }) => `
+   ${accordionCss({ dt })}
    ${avatarCss({ dt })}
    ${badgeCss({ dt })}
    ${breadcrumbCss({ dt })}
