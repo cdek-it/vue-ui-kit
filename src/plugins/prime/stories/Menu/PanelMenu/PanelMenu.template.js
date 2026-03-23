2b5b1b50015ab5dece46f1b9fdcc55f4d893b110
Danil Khaliulin <danielooneilie@gmail.com>
2026-03-23 14:48:01 +0700

    panelmenu: стилизация, сторисы

--- src/plugins/prime/stories/Menu/PanelMenu/PanelMenu.template.js
+++ src/plugins/prime/stories/Menu/PanelMenu/PanelMenu.template.js
@@ -1,19 +1,136 @@
-import { PanelMenu } from 'primevue';
-import CodeSnippet from '@/plugins/prime/stories/_common/CodeSnippet.vue';
-import { useMenu } from '@/plugins/prime/stories/Menu/_common/composables/useMenu';
+import { PanelMenu, Badge } from 'primevue';
+import { ref, computed } from 'vue';
 
-export const Template = (args) => ({
-  components: { PanelMenu, CodeSnippet },
+const createItems = (showIcons) => [
+  {
+    label: 'Home',
+    icon: showIcons ? 'ti ti-home' : undefined,
+    items: [
+      { label: 'Components', icon: showIcons ? 'ti ti-puzzle' : undefined },
+      { label: 'Blocks', icon: showIcons ? 'ti ti-layout' : undefined },
+      { label: 'UI Kit', icon: showIcons ? 'ti ti-palette' : undefined },
+      {
+        label: 'Templates',
+        icon: showIcons ? 'ti ti-template' : undefined,
+        items: [
+          { label: 'Apollo', icon: showIcons ? 'ti ti-rocket' : undefined },
+          { label: 'Ultima', icon: showIcons ? 'ti ti-star' : undefined },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Features',
+    icon: showIcons ? 'ti ti-star' : undefined,
+  },
+  {
+    label: 'Projects',
+    icon: showIcons ? 'ti ti-folder' : undefined,
+    items: [
+      { label: 'Components', icon: showIcons ? 'ti ti-puzzle' : undefined },
+      { label: 'Blocks', icon: showIcons ? 'ti ti-layout' : undefined },
+      { label: 'UI Kit', icon: showIcons ? 'ti ti-palette' : undefined },
+    ],
+  },
+  {
+    label: 'Contact',
+    icon: showIcons ? 'ti ti-user' : undefined,
+    disabled: true,
+  },
+];
+
+export const BasicTemplate = (args) => ({
+  components: { PanelMenu },
   setup() {
-    const { items } = useMenu();
+    const items = computed(() => createItems(args.showIcons));
 
     return { args, items };
   },
   template: `
-    <div>
-      <PanelMenu :model="items" multiple/>
-      <br>
-      <CodeSnippet :code="items"/>
-    </div>
-`,
+    <PanelMenu :model="items" />
+  `,
+});
+
+export const MultipleTemplate = (args) => ({
+  components: { PanelMenu },
+  setup() {
+    const items = computed(() => createItems(args.showIcons));
+
+    return { args, items };
+  },
+  template: `
+    <PanelMenu :model="items" multiple />
+  `,
+});
+
+export const CustomTemplate = (args) => ({
+  components: { PanelMenu, Badge },
+  setup() {
+    const items = ref([
+      {
+        label: 'Dashboard',
+        icon: 'ti ti-layout-dashboard',
+        description: 'Главная страница',
+        items: [
+          {
+            label: 'Analytics',
+            icon: 'ti ti-chart-line',
+            description: 'Аналитика данных',
+          },
+          {
+            label: 'Reports',
+            icon: 'ti ti-file-analytics',
+            description: 'Отчёты',
+          },
+          {
+            label: 'Statistics',
+            icon: 'ti ti-chart-bar',
+            description: 'Статистика',
+          },
+        ],
+      },
+      {
+        label: 'Features',
+        icon: 'ti ti-star',
+        description: 'Explore features',
+        badge: 'New',
+      },
+      {
+        label: 'Files',
+        icon: 'ti ti-folder',
+        description: 'Файловое хранилище',
+        items: [
+          {
+            label: 'Documents',
+            icon: 'ti ti-file-text',
+            description: 'Документы',
+          },
+          { label: 'Photos', icon: 'ti ti-photo', description: 'Фотографии' },
+        ],
+      },
+      {
+        label: 'Settings',
+        icon: 'ti ti-settings',
+        description: 'Настройки приложения',
+        disabled: true,
+      },
+    ]);
+
+    return { args, items };
+  },
+  template: `
+    <PanelMenu :model="items" multiple v-bind="args">
+      <template #item="{ item, props, hasSubmenu }">
+        <a v-bind="props.action" class="p-panelmenu-item-link">
+          <span v-if="item.icon" :class="['p-panelmenu-item-icon', item.icon]" />
+          <div class="panelmenu-item-label">
+            <span class="p-panelmenu-item-label">{{ item.label }}</span>
+            <small v-if="item.description" class="panelmenu-item-caption">{{ item.description }}</small>
+          </div>
+          <Badge v-if="item.badge" :value="item.badge" />
+          <span v-if="hasSubmenu" class="p-panelmenu-submenu-icon ti ti-chevron-right" />
+        </a>
+      </template>
+    </PanelMenu>
+  `,
 });
--- src/plugins/prime/theme3.0/css.ts
+++ src/plugins/prime/theme3.0/css.ts
@@ -6,6 +6,7 @@ import checkboxCss from './components/css/checkbox';
 import dialogCss from './components/css/dialog';
 import dividerCss from './components/css/divider';
 import drawerCss from './components/css/drawer';
+import panelmenuCss from './components/css/panelmenu';
 import progressbarCss from './components/css/progressbar';
 import ratingCss from './components/css/rating';
 import tagCss from './components/css/tag';
@@ -20,6 +21,7 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
    ${dialogCss({ dt })}
    ${dividerCss({ dt })}
    ${drawerCss({ dt })}
+   ${panelmenuCss({ dt })}
    ${progressbarCss({ dt })}
    ${ratingCss({ dt })}
    ${tagCss({ dt })}
@@ -28,43 +30,14 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
   .p-disabled, .p-component:disabled {
     mix-blend-mode: luminosity;
   }
-  
+
   [class*="menu"].p-component {
     border: none;
   }
-  
+
   [class*="menu"][class*="item"]:not(:has([class*="icon"])) [class*="item-content"] > [class*="item"] {
     padding-top: 0.625rem;
     padding-bottom: 0.625rem;
-  }
-  
-  [class*="menu"][class*="item"].p-disabled, .p-panelmenu-panel > .p-disabled {
-    background: ${dt('ButtonDisabledDisabledButtonBg')};
-    border-radius: calc(${dt('menuItemMenuitemBorderRadius')});
-    border: 1px solid ${dt('buttonDisabledDisabledButtonBorderColor')};
-  }
-  
-  [class*="menu"][class*="item"].p-disabled  [class*="item-content"] > [class*="item"] {
-    padding-top: calc(0.5rem - 1px);
-    padding-bottom: calc(0.5rem - 1px);
-  }
-   
-  [class*="menu"][class*="item"]:not(.p-disabled).p-menuitem-checked > [class*="item-content"] [class*="icon"] {
-     color: ${dt('menu.item.menuitemActiveIconColor')};
-  }
-  
-  [class*="menu"][class*="item"].p-menuitem-checked > [class*="item-content"] {
-    background: ${dt('menuItemMenuitemActiveBg')};
-    color: ${dt('menuItemMenuitemActiveTextColor')};
-  }
-  
-  [class*="menu"][class*="item"].p-menuitem-checked > [class*="item-content"]:hover {
-    background: ${dt('menuItemMenuitemActiveHoverBg')};
-    color: ${dt('menuItemMenuitemActiveTextColor')};
-  }
-  
-  .p-disabled [class*="menu"][class*="icon"] {
-     color: ${dt('buttonDisabledDisabledButtonTextColor')};
   }
 `;
 
