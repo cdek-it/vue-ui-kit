9a10c1505d99103de3060f112d5b110285982fe8
Danil Khaliulin <danieloonielie@gmail.com>
2026-03-25 16:31:51 +0700

    toggleswitch: стилизация, сторисы, тёмная тема (#166)
    Co-authored-by: Danil Khaliulin <danielooneilie@gmail.com>

--- src/plugins/prime/theme3.0/css.ts
+++ src/plugins/prime/theme3.0/css.ts
@@ -20,6 +20,7 @@ import selectbuttonCss from './components/css/selectbutton';
 import stepperCss from './components/css/stepper';
 import tabsCss from './components/css/tabs';
 import tagCss from './components/css/tag';
+import toggleswitchCss from './components/css/toggleswitch';
 import tooltipCss from './components/css/tooltip';
 
 const css = ({ dt }: { dt: (token: string) => string }) => `
@@ -45,48 +46,20 @@ const css = ({ dt }: { dt: (token: string) => string }) => `
    ${stepperCss({ dt })}
    ${tabsCss({ dt })}
    ${tagCss({ dt })}
+   ${toggleswitchCss({ dt })}
    ${tooltipCss({ dt })}
 
   .p-disabled, .p-component:disabled {
     mix-blend-mode: luminosity;
   }
-  
+
   [class*="menu"].p-component {
     border: none;
   }
-  
+
   [class*="menu"][class*="item"]:not(:has([class*="icon"])) [class*="item-content"] > [class*="item"] {
     padding-top: 0.625rem;
     padding-bottom: 0.625rem;
-  }
-  
-  [class*="menu"][class*="item"].p-disabled, .p-panelmenu-panel > .p-disabled {
-    background: ${dt('ButtonDisabledDisabledButtonBg')};
-    border-radius: calc(${dt('menuItemMenuitemBorderRadius')});
-    border: 1px solid ${dt('buttonDisabledDisabledButtonBorderColor')};
-  }
-  
-  [class*="menu"][class*="item"].p-disabled  [class*="item-content"] > [class*="item"] {
-    padding-top: calc(0.5rem - 1px);
-    padding-bottom: calc(0.5rem - 1px);
-  }
-   
-  [class*="menu"][class*="item"]:not(.p-disabled).p-menuitem-checked > [class*="item-content"] [class*="icon"] {
-     color: ${dt('menu.item.menuitemActiveIconColor')};
-  }
-  
-  [class*="menu"][class*="item"].p-menuitem-checked > [class*="item-content"] {
-    background: ${dt('menuItemMenuitemActiveBg')};
-    color: ${dt('menuItemMenuitemActiveTextColor')};
-  }
-  
-  [class*="menu"][class*="item"].p-menuitem-checked > [class*="item-content"]:hover {
-    background: ${dt('menuItemMenuitemActiveHoverBg')};
-    color: ${dt('menuItemMenuitemActiveTextColor')};
-  }
-  
-  .p-disabled [class*="menu"][class*="icon"] {
-     color: ${dt('buttonDisabledDisabledButtonTextColor')};
   }
 `;
 
