d2ad6cd869f38b57e677b95383b15306341634ef
Danil Khaliulin <danielooneilie@gmail.com>
2026-03-30 23:34:54 +0700

    inputnumber: стилизация, сторисы

--- src/plugins/prime/stories/Form/InputNumber/InputNumber.template.js
+++ src/plugins/prime/stories/Form/InputNumber/InputNumber.template.js
@@ -1,19 +1,45 @@
 import InputNumber from 'primevue/inputnumber';
+import FloatLabel from 'primevue/floatlabel';
+import { ref } from 'vue';
 
 export const Template = (args) => ({
   components: { InputNumber },
   setup() {
-    return { args };
+    const value = ref(null);
+    return { args, value };
   },
   template: `
-<div :style="{ display: 'grid', gridTemplateColumns: 'repeat(3, max-content)', gap: '15px', alignItems: 'center', justifyItems: 'center' }">
-  <span></span>
-  <span><code>invalid</code></span>
-  <span><code>disabled</code></span>
+    <InputNumber
+      v-model="value"
+      v-bind="args"
+    >
+      <template #incrementbuttonicon>
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 5v14M5 12h14"/></svg>
+      </template>
+      <template #decrementbuttonicon>
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/></svg>
+      </template>
+    </InputNumber>
+  `,
+});
 
-  <InputNumber v-bind="args" />
-  <InputNumber v-bind="args" invalid />
-  <InputNumber v-bind="args" disabled />
-</div>
-`,
+export const TemplateFloatLabel = (args) => ({
+  components: { InputNumber, FloatLabel },
+  setup() {
+    const value = ref(null);
+    return { args, value };
+  },
+  template: `
+    <FloatLabel variant="in">
+      <InputNumber id="number" v-model="value" showButtons buttonLayout="horizontal" v-bind="args">
+        <template #incrementbuttonicon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 5v14M5 12h14"/></svg>
+        </template>
+        <template #decrementbuttonicon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/></svg>
+        </template>
+      </InputNumber>
+      <label for="number">{{ args.label }}</label>
+    </FloatLabel>
+  `,
 });
