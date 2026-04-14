74c5bcbd5787bbd921a445c7ac6e153dd0d41bfe
Danil Khaliulin <danielooneilie@gmail.com>
2026-04-14 16:01:37 +0700

    перенёс стили disabled/checked из css.ts в обёртку

--- src/primeBlocks/PBlockTieredMenu/PBlockTieredMenu.vue
+++ src/primeBlocks/PBlockTieredMenu/PBlockTieredMenu.vue
@@ -12,3 +12,44 @@ defineOptions({ inheritAttrs: false });
 <template>
   <TieredMenu :model="model" v-bind="$attrs" />
 </template>
+
+<style>
+.p-tieredmenu.p-component .p-tieredmenu-item.p-disabled {
+  background: var(--p-button-disabled-disabled-button-bg);
+  border-radius: calc(var(--p-menu-item-menuitem-border-radius));
+  border: 1px solid var(--p-button-disabled-disabled-button-border-color);
+}
+
+.p-tieredmenu.p-component
+  .p-tieredmenu-item.p-disabled
+  .p-tieredmenu-item-content
+  > .p-tieredmenu-item-link {
+  padding-top: calc(0.5rem - 1px);
+  padding-bottom: calc(0.5rem - 1px);
+}
+
+.p-tieredmenu.p-component
+  .p-tieredmenu-item:not(.p-disabled).p-menuitem-checked
+  > .p-tieredmenu-item-content
+  .p-tieredmenu-item-icon {
+  color: var(--p-menu-item-menuitem-active-icon-color);
+}
+
+.p-tieredmenu.p-component
+  .p-tieredmenu-item.p-menuitem-checked
+  > .p-tieredmenu-item-content {
+  background: var(--p-menu-item-menuitem-active-bg);
+  color: var(--p-menu-item-menuitem-active-text-color);
+}
+
+.p-tieredmenu.p-component
+  .p-tieredmenu-item.p-menuitem-checked
+  > .p-tieredmenu-item-content:hover {
+  background: var(--p-menu-item-menuitem-active-hover-bg);
+  color: var(--p-menu-item-menuitem-active-text-color);
+}
+
+.p-tieredmenu.p-component .p-disabled .p-tieredmenu-item-icon {
+  color: var(--p-button-disabled-disabled-button-text-color);
+}
+</style>
