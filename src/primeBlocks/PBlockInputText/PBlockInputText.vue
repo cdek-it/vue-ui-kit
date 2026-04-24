8af21ba8359ec26b7ac92116bfca73e39c921540
Danil Khaliulin <danielooneilie@gmail.com>
2026-04-24 17:00:17 +0700

    PBlockInputText: подкомпонент PBlockInputTextField, устранение дублирования

--- src/primeBlocks/PBlockInputText/PBlockInputText.vue
+++ src/primeBlocks/PBlockInputText/PBlockInputText.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-import {
-  type InputTextProps,
-  FloatLabel,
-  IconField,
-  InputIcon,
-  InputText,
-} from 'primevue';
-import { IconX } from '@tabler/icons-vue';
-import { useAttrs } from 'vue';
+import { type InputTextProps, FloatLabel } from 'primevue';
+import PBlockInputTextField from './PBlockInputTextField.vue';
 
 interface PBlockInputTextProps extends /* @vue-ignore */ InputTextProps {
   modelValue?: string;
@@ -19,105 +12,58 @@ interface PBlockInputTextProps extends /* @vue-ignore */ InputTextProps {
   fluid?: boolean;
 }
 
-const props = withDefaults(defineProps<PBlockInputTextProps>(), {
+withDefaults(defineProps<PBlockInputTextProps>(), {
   showClear: true,
 });
 
-defineOptions({ inheritAttrs: false });
-
-const attrs = useAttrs();
-
-const emit = defineEmits<{
+defineEmits<{
   (e: 'update:modelValue', value: string): void;
   (e: 'value-change', value: string): void;
 }>();
-
-const onUpdateModelValue = (value: string) => {
-  emit('update:modelValue', value);
-  emit('value-change', value);
-};
-
-const onClear = () => {
-  emit('update:modelValue', '');
-  emit('value-change', '');
-};
 </script>
 
 <template>
   <FloatLabel v-if="hasFloatlabel" variant="in">
-    <IconField
-      class="p-block-inputtext"
-      :class="{ 'p-block-inputtext--fluid': fluid }"
+    <PBlockInputTextField
+      :id="label"
+      :modelValue="modelValue"
+      :showClear="showClear"
+      :size="size"
+      :fluid="fluid"
+      variant="filled"
+      v-bind="$attrs"
+      @update:modelValue="$emit('update:modelValue', $event)"
+      @value-change="$emit('value-change', $event)"
     >
-      <InputText
-        v-bind="attrs"
-        :id="label"
-        :modelValue="modelValue"
-        :fluid="fluid"
-        variant="filled"
-        :size="size === 'xlarge' ? undefined : size"
-        :class="{ 'p-inputtext-xlg': size === 'xlarge' }"
-        @update:modelValue="onUpdateModelValue($event as string)"
-      />
-      <InputIcon
-        v-show="showClear && modelValue"
-        class="p-block-inputtext__icon"
-        @click.stop="onClear"
-      >
-        <slot name="clear-icon">
-          <IconX :size="14" />
-        </slot>
-      </InputIcon>
-    </IconField>
+      <template #clear-icon>
+        <slot name="clear-icon" />
+      </template>
+    </PBlockInputTextField>
     <label :for="label">
       {{ label
       }}<span v-if="required" class="p-block-inputtext__required">*</span>
     </label>
   </FloatLabel>
 
-  <IconField
+  <PBlockInputTextField
     v-else
-    class="p-block-inputtext"
-    :class="{ 'p-block-inputtext--fluid': fluid }"
+    :modelValue="modelValue"
+    :showClear="showClear"
+    :size="size"
+    :fluid="fluid"
+    v-bind="$attrs"
+    @update:modelValue="$emit('update:modelValue', $event)"
+    @value-change="$emit('value-change', $event)"
   >
-    <InputText
-      v-bind="attrs"
-      :modelValue="modelValue"
-      :fluid="fluid"
-      :size="size === 'xlarge' ? undefined : size"
-      :class="{ 'p-inputtext-xlg': size === 'xlarge' }"
-      @update:modelValue="onUpdateModelValue($event as string)"
-    />
-    <InputIcon
-      v-show="showClear && modelValue"
-      class="p-block-inputtext__icon"
-      @click.stop="onClear"
-    >
-      <slot name="clear-icon">
-        <IconX :size="14" />
-      </slot>
-    </InputIcon>
-  </IconField>
+    <template #clear-icon>
+      <slot name="clear-icon" />
+    </template>
+  </PBlockInputTextField>
 </template>
 
 <style scoped lang="scss">
-.p-block-inputtext {
-  :deep(.p-inputtext) {
-    width: 100%;
-  }
-
-  &--fluid {
-    width: 100%;
-  }
-
-  &__icon {
-    cursor: pointer;
-    z-index: 1;
-  }
-
-  &__required {
-    color: var(--p-red-500);
-    margin-left: var(--p-spacing-1x);
-  }
+.p-block-inputtext__required {
+  color: var(--p-red-500);
+  margin-left: var(--p-spacing-1x);
 }
 </style>
