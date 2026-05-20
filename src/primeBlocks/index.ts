fd5f0c03b5b069bfa808e490d3d9630780ced343
Danil Khaliulin <danielooneilie@gmail.com>
2026-05-20 11:03:23 +0700

    toast: экспорт usePBlockToast и PBlockToastMessageIcon из primeBlocks/index.ts

--- src/primeBlocks/index.ts
+++ src/primeBlocks/index.ts
@@ -3,3 +3,7 @@ import PBlockToggleButton from './PBlockToggleButton/PBlockToggleButton.vue';
 import PBlockToast from './PBlockToast/PBlockToast.vue';
 
 export { PBlockPassword, PBlockToggleButton, PBlockToast };
+export {
+  usePBlockToast,
+  PBlockToastMessageIcon,
+} from './PBlockToast/usePBlockToast';
